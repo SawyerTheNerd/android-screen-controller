26998de45f952738a7efef873744bee2a2994402
enzonic-admin <admin@enzonic.com>
2026-02-06 19:49:10 -0500

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@yume-chan/fetch-scrcpy-server": path.resolve(
+        __dirname,
+        "node_modules/@yume-chan/fetch-scrcpy-server/index.js"
+      ),
+    },
+  },
   optimizeDeps: {
     exclude: [
       "@yume-chan/fetch-scrcpy-server",
